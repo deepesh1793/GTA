25ec58d254c05af259d35ce0ae242c3be09e0f7e
deepesh1793 <111624070+deepesh1793@users.noreply.github.com>
2023-10-27 10:30:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar";
+import Patent from "./components/Patent";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Patent/>
+
+      </>
+  
+  );
+}
+
+export default App;
